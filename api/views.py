2176bcc44eab9b8e0e1c1341db152252349025a8
Duclair TIOTSOP LAMBOU <tioladuc@yahoo.fr>
2026-09-19 05:32:49 -0400

    Initial Django project setup

---
+++ api/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+
+@api_view(['GET'])
+def hello_api(request):
+    return Response({
+        "success": True,
+        "message": "Django API is working"
+    })
+
+
